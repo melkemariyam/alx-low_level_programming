f562eef5159c983e63100d49ca28c20d4c33e442
melkemariyam <samulili.hunt@gmail.com>
2023-02-16 00:25:51 -0800

    commited out

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main-This is the main fuction description
+ *
+ *Return: 0 for succesful exit
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
