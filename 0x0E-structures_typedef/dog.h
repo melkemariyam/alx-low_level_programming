f1edf40a4232aeef506599376efb102ee15dc360
melkemariyam <samulili.hunt@gmail.com>
2023-03-20 03:45:48 -0700

    second commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,22 +2,19 @@
 #define DOG_H
 
 /**
- * dog_t - Typedef for struct dog
+ * struct dog - struture of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
-typedef struct dog dog_t;
 
-/**
- * struct dog - structure for dogs
- * @name: first member
- * @age: second member
- * @owner: third member
- */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+
+} dog_t;
 
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -25,5 +22,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif /* #ifndef DOG_H */
-
 
