2dbf8932e7620bceba3f9a46accd4a0ebcaeb327
melkemariyam <samulili.hunt@gmail.com>
2023-05-22 04:51:26 -0700

    again

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/sentayehut/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libinjection.so
-export LD_PRELOAD="$PWD/../libinjection.so"
+wget -P .. https://github.com/melkemariyam/alx-low_level_programming/tree/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
